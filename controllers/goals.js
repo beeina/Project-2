5eae754c3f7990ccbdcd01e788fb5cdeb41f73cb
bina <beeina@hotmail.com>
2023-07-18 23:08:12 -0700

    fix delete goals

--- controllers/goals.js
+++ controllers/goals.js
@@ -1,9 +1,24 @@
 const Habit = require("../models/habit");
 const Goal = require("../models/goal");
+const habit = require("../models/habit");
+const goal = require("../models/goal");
 
 module.exports = {
   create,
+  delete: deleteGoal,
 };
+
+async function deleteGoal(req, res) {
+  const habit = await Habit.findById(req.params.habitId);
+  // console.log(habit);
+  // console.log(req.params);
+  // Rogue user!
+  if (!habit) return res.redirect("/habits");
+  // Remove the goals using the remove method available on Mongoose arrays
+  habit.goal.remove(req.params.goalId);
+  await habit.save();
+  res.redirect(`/habits/${habit._id}`);
+}
 
 async function create(req, res) {
   const goal = await Goal.create(req.body);
--- routes/goals.js
+++ routes/goals.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const goalsCtrl = require('../controllers/goals');
-const ensureLoggedIn = require('../config/ensureLoggedIn');
+const goalsCtrl = require("../controllers/goals");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
+// POST /habits/:id/goals (create goal for habit)
+router.post("/habits/:id/goal", ensureLoggedIn, goalsCtrl.create);
 
-router.post('/habits/:id/goal', ensureLoggedIn, goalsCtrl.create);
-
+// DELETE /goals
+router.delete("/goals/:habitId/:goalId", ensureLoggedIn, goalsCtrl.delete);
 
 module.exports = router;
