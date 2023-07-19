8d0566202f12b3916eada8651b98701b40fea1cf
bina <beeina@hotmail.com>
2023-07-19 07:21:47 -0700

    edit and update goal

--- controllers/goals.js
+++ controllers/goals.js
@@ -1,12 +1,24 @@
 const Habit = require("../models/habit");
 const Goal = require("../models/goal");
-const habit = require("../models/habit");
-const goal = require("../models/goal");
 
 module.exports = {
   create,
   delete: deleteGoal,
+  edit,
+  update,
+  // update
 };
+
+async function update(req, res) {
+  const goal = await Goal.findById(req.params.goalId);
+  const habit = await Habit.findById(req.params.habitId);
+  goal.goalDescription = req.body.goalDescription;
+  goal.progress = req.body.progress;
+  // console.log(goal);
+  await goal.save();
+  updateHabit(habit, goal.progress);
+  res.redirect(`/habits`);
+}
 
 async function deleteGoal(req, res) {
   const habit = await Habit.findById(req.params.habitId);
@@ -27,11 +39,29 @@ async function create(req, res) {
   const habit = await Habit.findById(req.params.id);
 
   habit.goal.push(goal._id);
+  updateHabit(habit, goal.progress);
+  res.redirect(`/habits/${habit._id}`);
+}
+
+async function edit(req, res) {
+  const goal = await Goal.findById(req.params.goalId);
+  const habit = await Habit.findById(req.params.habitId);
+  // console.log(goal);
+  // const goal = habit.goals.id(req.params.id);
+  // Render the comments/edit.ejs template, passing to it the comment
+  res.render("goals/edit", { title: "Edit Goal", goal: goal, habit: habit });
+}
+
+async function updateHabit(habit, goalProgress) {
+  let totalProgress = habit.progress + goalProgress;
+
+  console.log(totalProgress);
+  console.log(habit.goal.length);
+  habit.progress = totalProgress / habit.goal.length;
 
   try {
     await habit.save();
   } catch (err) {
     console.log(err);
   }
-  res.redirect(`/habits/${habit._id}`);
 }
--- routes/goals.js
+++ routes/goals.js
@@ -6,7 +6,10 @@ const ensureLoggedIn = require("../config/ensureLoggedIn");
 // POST /habits/:id/goals (create goal for habit)
 router.post("/habits/:id/goal", ensureLoggedIn, goalsCtrl.create);
 
+router.put("/goals/update/:habitId/:goalId", ensureLoggedIn, goalsCtrl.update);
 // DELETE /goals
 router.delete("/goals/:habitId/:goalId", ensureLoggedIn, goalsCtrl.delete);
+
+router.get("/goals/edit/:habitId/:goalId", goalsCtrl.edit);
 
 module.exports = router;
